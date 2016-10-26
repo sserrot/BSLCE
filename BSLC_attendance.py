34ff99e4510a9d8cd046c31caea70b0bc7de4d19
Santiago Torres <santiago6695@gmail.com>
2016-10-26 18:23:49 -0400

    Fixed some openpyxl bugs

--- BSLC_attendance.py
+++ BSLC_attendance.py
@@ -1,3 +1,4 @@
+import openpyxl
 from openpyxl import load_workbook # import openpyxl for excel manipulation
 import smtplib # import smtplib to send email
 
@@ -7,27 +8,33 @@ nonAttendantStudents = {}
 
 
 def excel_parse(file):
-    wb = load_workbook(file)
-    sheet = wb['Sheet1']
+    wb = load_workbook(file, use_iterators=True)
+    sheet = wb.get_sheet_by_name(name='Sheet1')
     i = 1
-    for row in sheet.iter_cols(4):  # check column 4 for attendance
-        for cell in row:
-            if str(sheet['D'+str(i)].value).upper() == 'MISS':  # check if person attended
-                full_name = sheet['A'+str(i)].value   # store full name
-                last = full_name.rsplit(' ')[0].strip(',')  # format - broken if MISS is empty
-                if len(full_name.rsplit(' ')) == 1:
+    for row in sheet.iter_rows():
+        if sheet.cell(row=i, column=1).value is not None:
+            for cell in row:
+                print(i)
+                print(sheet.cell(row=i, column=1).value)
+                if str(sheet.cell(row=i, column=4).value).upper() == 'MISS':  # check if person attended
+                    full_name = sheet.cell(row=i, column=1).value  # store full name
+                    print(full_name)
+                    last = full_name.rsplit(' ')[0].strip(',')  # format - broken if MISS is empty
+                    if len(full_name.rsplit(' ')) == 1:
+                        continue
+                    else:
+                        first = full_name.rsplit(' ')[1]  # format, causes an error if not in LAST, FIRST format
+                        email = (
+                        last[:7] + '_' + first[:4] + '@bentley.edu')  # email format is 7last_2first@bentley.edu
+                        name = first + ' ' + last
+                        nonAttendantStudents[name] = email  # add to a dictionary
+                        i += 1
                         continue
                 else:
-                    first = full_name.rsplit(' ')[1]  # format, causes an error if not in LAST, FIRST format
-                    email = (last[:7] + '_' + first[:4] + '@bentley.edu')  # email format is 7last_2first@bentley.edu
-                    name = first + ' ' + last
-                    nonAttendantStudents[name] = email  # add to a dictionary
-                    i += 1
+                    i += 1  # increment where you are checking
                     continue
-            else:
-                i += 1  # increment where you are checking
-                continue
-
+        else:
+            break
 # create a text/plain message
 
 
@@ -50,7 +57,14 @@ def send_emails(sender_address, password, subject, text_body):
 
         if send_mail_status != {}:
             print('There was a problem sending email to %s: %s' % (email, send_mail_status))
+    all_body = "Subject:" + str(subject) + "\n" + "Dear BSLCE, \n\n"
+    for name in nonAttendantStudents.items():
+        all_body = all_body + ' ' + name[0] + ";"
 
+    send_all_status = smtp_obj.sendmail(sender_address, sender_address, all_body)
+
+    if send_all_status != {}:
+        print('There was a problem sending email to %s: %s' % (sender_address,send_all_status))
     smtp_obj.quit()
 
 
